80f24ca209dcef0d63d62bf38f984a3d671c09fe
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2024-09-10 17:32:35 +0530

    Deployment Script 04

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const HTTP_SERVER_PORT = 4500/* , SOCKET_IO_SERVER_PORT = 5000; */
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server,{
-    cors: true
-});
+const io = new Server(server,{cors: {origin: 'http://localhost:5173',   methods: ['GET', 'POST'],   credentials: true}});
 
 app.use(cors());
 app.use(urlencoded({ extended:true }))
